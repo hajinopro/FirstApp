0718070f7de829dcad35ed6c3e0bf23f6eef1f10
Nathan <hajinopro@gmail.com>
2021-08-21 13:17:56 +0900

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Nathan on 2021/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Nathan on 2021/08/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
